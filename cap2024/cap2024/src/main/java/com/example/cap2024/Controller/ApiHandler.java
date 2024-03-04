ba31adb864b8a6eaeac2f00908b9bb2a8630a3ba
나상연 <58220151+ense333@users.noreply.github.com>
2024-03-04 21:22:23 +0900

    Api Handler에서 Service2, Service3에 대해서도 view할 수 있도록 수정

--- cap2024/cap2024/src/main/java/com/example/cap2024/Controller/ApiHandler.java
+++ cap2024/cap2024/src/main/java/com/example/cap2024/Controller/ApiHandler.java
@@ -1,7 +1,11 @@
 package com.example.cap2024.Controller;
 
 import com.example.cap2024.BusinessLogicLayer.Service1;
+import com.example.cap2024.BusinessLogicLayer.Service2;
+import com.example.cap2024.BusinessLogicLayer.Service3;
 import com.example.cap2024.DataAccessLayer.dao1.Dao1;
+import com.example.cap2024.DataAccessLayer.dao2.Dao2;
+import com.example.cap2024.DataAccessLayer.dao3.Dao3;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,9 +20,13 @@ public class ApiHandler {
 
 
     Dao1 dao1 = new Dao1();
+    Dao2 dao2 = new Dao2();
+    Dao3 dao3 = new Dao3();
 
 
-    Service1 service = new Service1(dao1);
+    Service1 service1 = new Service1(dao1);
+    Service2 service2 = new Service2(dao2);
+    Service3 service3 = new Service3(dao3);
 
 
 
@@ -31,7 +39,27 @@ public class ApiHandler {
     @GetMapping("/getDataFromService1")
     public ResponseEntity getDataFromService1() {
         try {
-            String result = service.fetchDataFromDao();
+            String result = service1.fetchDataFromDao();
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Internal error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/getDataFromService2")
+    public ResponseEntity getDataFromService2() {
+        try {
+            String result = service2.fetchDataFromDao();
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Internal error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/getDataFromService3")
+    public ResponseEntity getDataFromService3() {
+        try {
+            String result = service3.fetchDataFromDao();
             return new ResponseEntity<>(result, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>("Internal error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
